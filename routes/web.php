85267a936e4895395ceaf6d26144c5bae6675b82
qingfeng1997 <1373823008@qq.com>
2019-05-09 16:20:51 +0000

    用户注册界面

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,5 @@
 Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/fap','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
+
+Route::get('signup','UsersController@create')->name('signup');
