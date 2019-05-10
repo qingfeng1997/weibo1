e5939ee4457db804e7a57e8c8cb2923b0f1a28ee
qingfeng1997 <1373823008@qq.com>
2019-05-10 14:28:48 +0000

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -2,28 +2,31 @@
 @section('title', '登录')
 
 @section('content')
-<div class="offset-md-2 col-md-8">
-  <div class="card ">
-    <div class="card-header">
+<div class="col-md-offset-2 col-md-8">
+  <div class="panel panel-default">
+    <div class="panel-heading">
       <h5>登录</h5>
     </div>
-    <div class="card-body">
-      @include('shared._errors')
+    <div class="panel-body">
 
       <form method="POST" action="{{ route('login') }}">
-          {{ csrf_field() }}
+        {{ csrf_field() }}
 
-          <div class="form-group">
-            <label for="email">邮箱：</label>
-            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
-          </div>
+        <div class="form-group">
+          <label for="email">邮箱：</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
 
-          <div class="form-group">
-            <label for="password">密码：</label>
-            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-          </div>
+        <div class="form-group">
+          <label for="password">密码：</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
 
-          <button type="submit" class="btn btn-primary">登录</button>
+        <div class="checkbox">
+          <label><input type="checkbox" name="remember"> 记住我</label>
+        </div>
+
+        <button type="submit" class="btn btn-primary">登录</button>
       </form>
 
       <hr>
