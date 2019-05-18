47829f152eea6b550c80db4da5af7c594b7e165f
qingfeng1997 <1373823008@qq.com>
2019-05-18 06:44:51 +0000

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,10 @@ class User extends Authenticatable
 		    $user->activation_token = Str::random(10);
 	    });
     }
+
+    //指明一个用户拥有多条微博
+    public function statuses()
+    {
+	    return $this->hasMany(Status::class);
+    }
 }
